402ade06d7d82524d22f41bc745d1a83cbce91e6
Ayane-2030 <rianigorsa@gmail.com>
2025-05-21 11:56:53 -0300

    mensagem

--- wolf_store/index.php
+++ wolf_store/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="Rian I. Silva" />
     <title>Wolf Store</title>
-    <link rel="stylesheet" href="css/styles.css"><!-- Conectar uma folha css em outra pasta -->
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" type="image/x-icon" href="imgs/wolf_store_logo.jpg"><!-- Adicionar um icon no guia da página -->
     <?php include_once('conn/conn.php');?>
 </head>
